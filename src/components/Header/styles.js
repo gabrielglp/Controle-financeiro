f7d8fb84faa51077d0578c85806256137201c6da
Gabriel de Souza Martins <83505259+gabrielglp@users.noreply.github.com>
2022-07-19 20:17:40 -0300

    Estilizando e marcando
    Não concluido.

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
+// alinhamento dos blocos com a cor de fundo
 export const Container = styled.div`
   height: 150px;
   text-align: center;
@@ -8,6 +9,7 @@ export const Container = styled.div`
 
 export const Header = styled.h1``
 
+// alinhamento do titulo (Controle financeiro)
 export const Title = styled.div`
   padding-top: 20px;
   color: #fff;
